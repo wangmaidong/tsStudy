223a0cf57c3ef863c99b4c875b34d656e1882df8
wanglei <2934490605@qq.com>
2023-10-23 19:55:12 +0800

    feat:  练习

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,24 @@ let arr3: boolean[] = [true, false]
 let arr4: Array<number | string> = ['1', 2]
 let arr5: (number | string)[] = ['123', 23]
 enum USER_ROLE {
-  USER,
-  ADMIN,
-  MANAGER
+  USER = 10,
+  ADMIN = 'string',
+  MANAGER = 'sbc'
 }
-console.log(USER_ROLE)
+let arr:any = ['jiagou', true, 123]
+let test1:number | string
+test1 = 2
+let test2:void
+test2 = undefined // 严格模式下undefined可以赋值给void，但是null
+
+let test3
+test3 = 123
+
+let test4 = 'abc' // ji
+test4 = 'bcd'
+
+let bool1:boolean = false
+let bool2:boolean = Boolean(1)
+let bool3:Boolean = new Boolean(23)
+
+let str1:String = 'abc'
